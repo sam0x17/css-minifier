f28bf3321f76b34558d611142021324713a3de68
Sam Johnson <sam@durosoft.com>
2019-10-01 09:35:19 -0400

    basic minification working

--- src/css-minifier.cr
+++ src/css-minifier.cr
@@ -2,7 +2,19 @@ require "duktape"
 require "./clean-css"
 
 module CssMinifier
+  def self.initialize_context
+    ctx = Duktape::Sandbox.new
+    ctx.eval!(CLEAN_CSS_JS)
+    ctx
+  end
+  @@ctx : Duktape::Sandbox = initialize_context
 
+  def self.minify!(source : String)
+    source = source.gsub("\"", "\\\"")
+    source = source.gsub("\n", "\\n")
+    source = source.gsub("'", "\\'")
+    @@ctx.eval!("var output = new CleanCSS({}).minify(\'#{source}\');")
+    @@ctx.eval!("output.styles")
+    output = @@ctx.get_string(-1)
+  end
 end
-
-puts CLEAN_CSS_JS
