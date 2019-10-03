4cf82e9f70aa3ad3a665c12e906a973f7b13352c
Sam Johnson <sam@durosoft.com>
2019-10-03 13:56:44 -0400

    don't load clean-css js until it is needed

--- src/css-minifier.cr
+++ src/css-minifier.cr
@@ -7,17 +7,19 @@ module CssMinifier
     ctx.eval!(CLEAN_CSS_JS)
     ctx
   end
-  @@ctx : Duktape::Sandbox = initialize_context
+  @@ctx : Duktape::Sandbox? = nil
 
   def self.minify!(source : String)
+    @@ctx ||= initialize_context
+    ctx = @@ctx.not_nil!
     source = source.gsub("\"", "\\\"")
     source = source.gsub("\n", "\\n")
     source = source.gsub("'", "\\'")
-    @@ctx.eval!("var output = new CleanCSS({}).minify(\'#{source}\');")
-    @@ctx.eval!("output.styles")
-    output = @@ctx.get_string(-1).not_nil!
-    @@ctx.eval!("JSON.stringify(output.errors)")
-    errors = @@ctx.get_string(-1).not_nil!
+    ctx.eval!("var output = new CleanCSS({}).minify(\'#{source}\');")
+    ctx.eval!("output.styles")
+    output = ctx.get_string(-1).not_nil!
+    ctx.eval!("JSON.stringify(output.errors)")
+    errors = ctx.get_string(-1).not_nil!
     raise errors if errors != "[]"
     output
   end
